ec454d1da55c5f59c45acd3e038428e10e49092c
Pedro Carvalho <pedrohj.carvalho@gmail.com>
2021-07-04 20:41:22 -0400

    Adding the base studying project

---
+++ base-program/src/test/java/com/pedrohjc/BaseProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrohjc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
