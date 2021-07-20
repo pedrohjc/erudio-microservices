5268726cefd770034060c9f926c99c02a54bda48
Pedro Carvalho <pedrohj.carvalho@gmail.com>
2021-07-20 15:21:35 -0400

    adding Resilience4j

---
+++ 05-book-service/src/main/java/br/com/erudio/controller/FooBarController.java
@@ -0,0 +1,18 @@
+package br.com.erudio.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("book-service")
+public class FooBarController {
+	
+	@GetMapping("/foo-bar")
+	public String fooBar() {
+		new RestTemplate()
+			.getForEntity("http://localhost:8080/foo-bar", String.class);
+		return "Foo-Bar!!";
+	}
+}
